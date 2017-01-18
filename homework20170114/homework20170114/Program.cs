c4b944d544d5aaf6ea82b3268e7c18ac1037ce5d
ikerw <wangzhelun2014@gmail.com>
2017-01-18 15:53:42 -0600

    Add files via upload

---
+++ homework20170114/homework20170114/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace homework20170114
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//int[] myArray = { 8, 4, -3, 12, -43, 6 };
+
+			//int length = myArray.Length;
+			//int max = myArray[0];
+
+
+			// 1. select the max number from an int array
+			//for (int i = 0; i < length; i++)
+			//{
+			//	if (max < myArray[i])
+			//	{
+			//		max = myArray[i];
+			//	}
+			//}
+
+			// 2. compute the sum of an array 
+			//int sum = 0;
+			//for (int i = 0; i < length; i++)
+			//{
+			//	sum = sum + myArray[i];
+			//}
+
+			// 3. print a string with ‘-’ link all the string items
+			//string[] newArray = { "1", "2", "3", "4", "5", "6" };
+			//for (int i = 0; i < newArray.Length; i++)
+			//{
+			//	newArray[i] = newArray[i] + "-";
+			//	Console.Write(newArray[i]);
+
+			//}
+
+			// 4. add one for positive; subtract 1 for negative
+
+			//for (int i = 0; i < length; i++)
+			//{
+			//	if (myArray[i] >= 0)
+			//	{
+			//		myArray[i] = myArray[i] + 1;
+			//	}
+			//	else
+			//	{
+			//		myArray[i] = myArray[i] - 1;
+			//	}
+			//	Console.Write(myArray[i] + " ");
+			//}
+
+			// 5. convert int to string
+			//string[] newArray = new string[myArray.Length];
+			//for (int i = 0; i < length; i++)
+			//{
+			//	int temp = myArray[i];
+			//	string temp_1 = temp + "";
+			//	newArray[i] = temp_1;
+			//	Console.Write(newArray[i] + " ");
+			//}
+
+
+			// 6. reverse string array
+			//string[] newArray = { "1", "2", "3", "4", "5", "6" };
+			//string[] reverseArray = new string[newArray.Length];
+			//for (int i = 0; i < length; i++)
+			//{
+			//	reverseArray[i] = newArray[newArray.Length - i - 1];
+			//	Console.Write(reverseArray[i] + " ");
+			//}
+
+			public int 
+
+		}
+	}
+}
