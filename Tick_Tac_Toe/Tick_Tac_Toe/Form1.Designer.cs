cb2534ed6584250898abc96eb1381789506baf90
ikerw <wangzhelun2014@gmail.com>
2017-01-29 17:59:32 -0600

    Add files via upload

---
+++ Tick_Tac_Toe/Tick_Tac_Toe/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Tick_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.po1 = new System.Windows.Forms.Label();
+            this.po2 = new System.Windows.Forms.Label();
+            this.po3 = new System.Windows.Forms.Label();
+            this.po6 = new System.Windows.Forms.Label();
+            this.po5 = new System.Windows.Forms.Label();
+            this.po4 = new System.Windows.Forms.Label();
+            this.po9 = new System.Windows.Forms.Label();
+            this.po8 = new System.Windows.Forms.Label();
+            this.po7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // po1
+            // 
+            this.po1.AutoSize = true;
+            this.po1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po1.Location = new System.Drawing.Point(35, 32);
+            this.po1.Name = "po1";
+            this.po1.Size = new System.Drawing.Size(66, 91);
+            this.po1.TabIndex = 0;
+            this.po1.Text = "-";
+            this.po1.Click += new System.EventHandler(this.po1_Click);
+            // 
+            // po2
+            // 
+            this.po2.AutoSize = true;
+            this.po2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po2.Location = new System.Drawing.Point(167, 32);
+            this.po2.Name = "po2";
+            this.po2.Size = new System.Drawing.Size(66, 91);
+            this.po2.TabIndex = 1;
+            this.po2.Text = "-";
+            this.po2.Click += new System.EventHandler(this.po2_Click);
+            // 
+            // po3
+            // 
+            this.po3.AutoSize = true;
+            this.po3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po3.Location = new System.Drawing.Point(303, 32);
+            this.po3.Name = "po3";
+            this.po3.Size = new System.Drawing.Size(66, 91);
+            this.po3.TabIndex = 2;
+            this.po3.Text = "-";
+            this.po3.Click += new System.EventHandler(this.po3_Click);
+            // 
+            // po6
+            // 
+            this.po6.AutoSize = true;
+            this.po6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po6.Location = new System.Drawing.Point(303, 139);
+            this.po6.Name = "po6";
+            this.po6.Size = new System.Drawing.Size(66, 91);
+            this.po6.TabIndex = 5;
+            this.po6.Text = "-";
+            this.po6.Click += new System.EventHandler(this.po6_Click);
+            // 
+            // po5
+            // 
+            this.po5.AutoSize = true;
+            this.po5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po5.Location = new System.Drawing.Point(167, 139);
+            this.po5.Name = "po5";
+            this.po5.Size = new System.Drawing.Size(66, 91);
+            this.po5.TabIndex = 4;
+            this.po5.Text = "-";
+            this.po5.Click += new System.EventHandler(this.po5_Click);
+            // 
+            // po4
+            // 
+            this.po4.AutoSize = true;
+            this.po4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po4.Location = new System.Drawing.Point(35, 139);
+            this.po4.Name = "po4";
+            this.po4.Size = new System.Drawing.Size(66, 91);
+            this.po4.TabIndex = 3;
+            this.po4.Text = "-";
+            this.po4.Click += new System.EventHandler(this.po4_Click);
+            // 
+            // po9
+            // 
+            this.po9.AutoSize = true;
+            this.po9.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po9.Location = new System.Drawing.Point(303, 250);
+            this.po9.Name = "po9";
+            this.po9.Size = new System.Drawing.Size(66, 91);
+            this.po9.TabIndex = 8;
+            this.po9.Text = "-";
+            this.po9.Click += new System.EventHandler(this.po9_Click);
+            // 
+            // po8
+            // 
+            this.po8.AutoSize = true;
+            this.po8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po8.Location = new System.Drawing.Point(167, 250);
+            this.po8.Name = "po8";
+            this.po8.Size = new System.Drawing.Size(66, 91);
+            this.po8.TabIndex = 7;
+            this.po8.Text = "-";
+            this.po8.Click += new System.EventHandler(this.po8_Click);
+            // 
+            // po7
+            // 
+            this.po7.AutoSize = true;
+            this.po7.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.po7.Location = new System.Drawing.Point(35, 250);
+            this.po7.Name = "po7";
+            this.po7.Size = new System.Drawing.Size(66, 91);
+            this.po7.TabIndex = 6;
+            this.po7.Text = "-";
+            this.po7.Click += new System.EventHandler(this.po7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 362);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 31);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 426);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.po9);
+            this.Controls.Add(this.po8);
+            this.Controls.Add(this.po7);
+            this.Controls.Add(this.po6);
+            this.Controls.Add(this.po5);
+            this.Controls.Add(this.po4);
+            this.Controls.Add(this.po3);
+            this.Controls.Add(this.po2);
+            this.Controls.Add(this.po1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label po1;
+        private System.Windows.Forms.Label po2;
+        private System.Windows.Forms.Label po3;
+        private System.Windows.Forms.Label po6;
+        private System.Windows.Forms.Label po5;
+        private System.Windows.Forms.Label po4;
+        private System.Windows.Forms.Label po9;
+        private System.Windows.Forms.Label po8;
+        private System.Windows.Forms.Label po7;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Tick_Tac_Toe/Tick_Tac_Toe/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tick_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public int[,] matrix =
+        {
+            {11, 12, 13 },
+            {14, 15, 16 },
+            {17, 18, 19 }
+        };
+
+        public bool[] ifClicked =
+        {
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        };
+            
+        public int count = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void po1_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[0] == false)
+            {
+                ifClicked[0] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[0, 0] = 1;
+                    po1.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[0, 0] = 2;
+                    po1.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+
+        }
+
+        private void po2_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[1] == false)
+            {
+                ifClicked[1] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[0, 1] = 1;
+                    po2.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[0, 1] = 2;
+                    po2.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po3_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[2] == false)
+            {
+                ifClicked[2] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[0, 2] = 1;
+                    po3.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[0, 2] = 2;
+                    po3.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po4_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[3] == false)
+            {
+                ifClicked[3] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[1, 0] = 1;
+                    po4.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[1, 0] = 2;
+                    po4.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po5_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[4] == false)
+            {
+                ifClicked[4] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[1, 1] = 1;
+                    po5.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[1, 1] = 2;
+                    po5.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po6_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[5] == false)
+            {
+                ifClicked[5] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[1, 2] = 1;
+                    po6.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[1, 2] = 2;
+                    po6.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po7_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[6] == false)
+            {
+                ifClicked[6] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[2, 0] = 1;
+                    po7.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[2, 0] = 2;
+                    po7.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po8_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[7] == false)
+            {
+                ifClicked[7] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[2, 1] = 1;
+                    po8.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[2, 1] = 2;
+                    po8.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        private void po9_Click(object sender, EventArgs e)
+        {
+            if (ifClicked[8] == false)
+            {
+                ifClicked[8] = true;
+                count += 1;
+
+                if (count % 2 == 1)
+                {
+                    matrix[2, 2] = 1;
+                    po9.Text = "X";
+                }
+                else if (count % 2 == 0)
+                {
+                    matrix[2, 2] = 2;
+                    po9.Text = "O";
+                } 
+            }
+
+            bool result = evaluateGameResult(matrix);
+            if (result)
+            {
+                textBox1.Text = "Game Over";
+            }
+        }
+
+        static bool evaluateGameResult(int [,] matrix)
+        {
+            
+            bool[] cond = new bool[5];
+
+            // evaluate each row and each column, if any one of them has
+            // the same value, save true
+            for(int i = 0; i < 3; i++ )
+            {
+                bool cond1 = matrix[i, 0] == matrix[i, 1] && matrix[i, 1] == matrix[i, 2];
+                bool cond2 = matrix[0, i] == matrix[1, i] && matrix[1, i] == matrix[2, i];
+                cond[i] = cond1 || cond2;
+            }
+
+            // evaluate the diagonal entries
+            bool cond3 = matrix[0, 0] == matrix[1, 1] && matrix[1, 1] == matrix[2, 2];
+            bool cond4 = matrix[0, 2] == matrix[1, 1] && matrix[1, 1] == matrix[2, 0];
+
+            cond[3] = cond3;
+            cond[4] = cond4;
+
+            if(cond[0] || cond[1] || cond[2] || cond[3] || cond[4])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
